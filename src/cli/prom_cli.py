e7324d7530b9b3bbedec98997cdbbe3fdcff85dd
Thanner Soares Silva <thannersoares@gmail.com>
2021-03-04 22:55:36 -0300

    Swagger (docker not working)

--- src/cli/prom_cli.py
+++ src/cli/prom_cli.py
@@ -3,7 +3,6 @@ from os import path
 
 def execute_prom_cli(script, command):
     filepath = path.abspath(path.join(path.dirname(__file__), "..", "..", "prom", "script", script))
-    # TODO: Script funciona bem, mas o Prom está demorando
     output = subprocess.run(['sh', filepath, command], capture_output=True, text=True)
     return {"args": output.args, "stdout": output.stdout.split("\n"), "stderr": output.stderr.split("\n"),
             "returnCode": output.returncode}
